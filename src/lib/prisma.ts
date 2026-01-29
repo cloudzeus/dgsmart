db9a01f65299664c85c191444503743d2e4d9c61
Γιαννης `Κοζυρης <kozyris@macgiannis-2.local>
2026-01-29 09:13:40 +0200

    deployment issues with prisma 7 compatibility

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+const connectionString = process.env.DATABASE_URL || ''
+const adapter = new PrismaMariaDb(connectionString)
 
 export const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
+        adapter,
         log: ['query'],
     })
 
